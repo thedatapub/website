a511665ca8b4ed423afb26db9a793e0b22c82a8a
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-07-12 22:40:44 -0500

    Containerized

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 VER=$(grep "version" website.properties|cut -d'=' -f2)  && \
   docker build -t thedatapub/website:$VER \
     -t registry.circulosiete.com/library/thedatapub/website:$VER . && \
-  #git release $VER && \
-  #docker push thedatapub/website:$VER && \
-  #docker push registry.circulosiete.com/library/thedatapub/website:$VER && \
+  git release $VER && \
+  docker push thedatapub/website:$VER && \
+  docker push registry.circulosiete.com/library/thedatapub/website:$VER && \
   echo "done"
